9509d70d5a62374423c2d3b5da994348402f2468
Edward P. Legaspi <czetsuya@gmail.com>
2021-05-12 22:25:22 +0800

    feat: add request via nextjs api

--- pages/api/books.js
+++ pages/api/books.js
@@ -1,8 +1,6 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
 export default async (req, res) => {
 
-  const resp = await fetch(`http://localhost:8080/books`)
+  const resp = await fetch(`http://192.168.1.9:8080/books/?r=` + Math.random())
   const data = await resp.json()
 
   res.status(200).json({data})
--- pages/index.js
+++ pages/index.js
@@ -2,15 +2,24 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import React from "react";
 
+const API_URL = 'http://192.168.1.9:8080'
+
 const Home = () => {
 
   const [books, setBooks] = React.useState()
 
-  const getBooks = async () => {
+  const getBooksViaApi = async () => {
     const resp = await fetch('/api/books')
     const result = await resp.json()
     setBooks(result.data)
     console.log(result.data)
+  }
+
+  const getBooks = async () => {
+    const resp = await fetch(`${API_URL}/books`)
+    const result = await resp.json()
+    setBooks(result)
+    console.log(result)
   }
 
   return (
@@ -23,13 +32,15 @@ const Home = () => {
 
         <main className={styles.main}>
           <h1 className={styles.title}>
-            Spring CORS by Czetsuya Tech
+            Spring CORS
           </h1>
+          <p>by Czetsuya Tech</p>
 
           <div>
             <h2>Actions</h2>
             <div>
               <button onClick={getBooks}>Get Books</button>
+              <button onClick={getBooksViaApi}>Get Books via API</button>
             </div>
           </div>
 
@@ -42,6 +53,17 @@ const Home = () => {
                 </li>
             ))
             }
+          </div>
+
+          <div className={styles.grid}>
+            <div className={styles.card}>
+              If you would like to support these tutorials I have a Patreon account where you could
+              contribute. It is a service that allows supporters to contribute as little as a dollar a month.
+              I would be most grateful for any support. Also, if you have suggestions for future topics I
+              would love to hear your feedback.
+              <br/><br/>
+              <div>Patreon: <a target="_blank" href="https://www.patreon.com/czetsuya">https://www.patreon.com/czetsuya</a></div>
+            </div>
           </div>
         </main>
 
